23f380fbcb7c88574e86ce51b128c4a524507b3f
bhangdante@gmail.com <87205105+dantebhang@users.noreply.github.com>
2022-03-11 12:47:29 -0800

    added context and components

---
+++ src/contexts/ResultContextProvider.js
@@ -0,0 +1,48 @@
+import React, { createContext, useContext, useState } from "react";
+
+//pass this context to entire application
+
+const ResultContext = createContext();
+const baseUrl = "https://google-search3.p.rapidapi.com/api/v1";
+
+export const ResultContextProvider = ({ children }) => {
+	const [results, setResults] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [searchTerm, setSearchTerm] = useState("Elon Musk");
+
+	// for /videos /search or /images path
+	const getResults = async (urlType) => {
+		setIsLoading(true);
+
+		const response = await fetch(`${baseUrl}${urlType}`, {
+			method: "GET",
+			headers: {
+				"x-rapidapi-host": "google-search3.p.rapidapi.com",
+				"x-rapidapi-key": "7b3a0606bfmsh9c4e08ae5eca17ep10a2bejsn426c6d96a9f6",
+			},
+		});
+
+		const data = await response.json();
+
+		//combat the data key that is "entries" in news as it confuses the data fetching
+		if (urlType.includes("/news")) {
+			setResults(data.entries);
+		} else if (urlType.includes("/images")) {
+			setResults(data.image_results);
+		} else {
+			setResults(data.results);
+		}
+
+		setIsLoading(false);
+	};
+
+	return (
+		<ResultContext.Provider
+			value={{ getResults, results, searchTerm, setSearchTerm, isLoading }}
+		>
+			{children}
+		</ResultContext.Provider>
+	);
+};
+
+export const useResultContext = () => useContext(ResultContext);
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import { ResultContextProvider } from "./contexts/ResultContextProvider";
+
+ReactDOM.render(
+	<React.StrictMode>
+		<ResultContextProvider>
+			<Router>
+				<App />
+			</Router>
+		</ResultContextProvider>
+	</React.StrictMode>,
+	document.getElementById("root"),
+);
